655b1c52caa9c8cfbfc6b3c571fb3bef9c7d844c
dbdejss <2562252467@qq.com>
2022-01-16 23:51:17 +0800

    提交代码

---
+++ 2022-1-16/2022-1-16.4/2022-1-16.4/2022-1-16.4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+//链式访问：把一个函数的返回值作为另一个函数的参数
+//列：
+	//char arr[]="bit";
+	//1
+	//int ret = strlen(arr);
+	//printf("%d\n",ret);
+
+	//2
+	/*printf("%d\n",strlen(arr));*/
+
+//列：
+/*	printf("%d",printf("%d",printf("%d",43)));	*/	
+	//printf函数的返回值是字符的个数
+	//printf("%d",43)——43，printf("%d",2)——2，printf("%d",1)——1
+}

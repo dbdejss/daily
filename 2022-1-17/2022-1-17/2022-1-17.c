1ab76fa26a4d76c674cf03240aacaad256ad1f6d
dbdejss <2562252467@qq.com>
2022-01-17 22:53:24 +0800

    提交代码

---
+++ 2022-1-17/2022-1-17/2022-1-17.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "add.h"
+//int main()
+//{
+//	int a = 20;
+//	int b = 10;
+//	printf("sum = %d\n",Add(a,b));
+//	return 0;
+//}
+
+//int main()
+//{
+//递归函数
+//通过一个简单的递归来理解什么是递归函数
+//
+//	printf("你好\n");
+//	main();
+//	return 0;
+//}
+
+
+//void print(int n)
+//{
+//	if(n>9)
+//		print(n/10);
+//	printf("%d  ",n%10);
+//}
+//int main()
+//{
+////通过将整数1234打印成1 2 3 4来进一步理解递归
+//	int num = 1234;
+//	print(num);
+//	return 0;
+//}
+
+
